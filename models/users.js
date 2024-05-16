6210c1192e24fb7496b93e7dc2318f9ada0beda9
sheykhnazirboyev <sheykhnazirboyev@gmail.com>
2024-05-16 07:13:37 +0500

    feat: add auth middleware

--- models/users.js
+++ models/users.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign(
     { _id: this._id, role: this.role },
-    process.env.JWT_PRIVATE_KEY || "Test@123"
+    process.env.JWT_PRIVATE_KEY
   );
   return token;
 };
