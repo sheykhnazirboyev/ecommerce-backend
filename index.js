f9ef45e90c63859748b0f820376c3b5e469ae0f7
sheykhnazirboyev <sheykhnazirboyev@gmail.com>
2024-05-16 05:33:06 +0500

    add initial changes

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { homeRouter } from "./routes/home";
+
+const app = express();
+
+app.use(express.json());
+app.use("/", homeRouter);
+
+const port = process.env.PORT || 9001;
+
+app.listen(port, () => {
+    console.log("Server started on ", port)
+});
