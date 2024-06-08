bec844f728315d941e5db934085ca499168bf33b
sheykhnazirboyev <sheykhnazirboyev@gmail.com>
2024-06-08 16:29:05 +0500

    feat: remove auth from categories

--- routes/categories.js
+++ routes/categories.js
@@ -63,7 +63,7 @@ router.post("/", [auth], async (req, res, next) => {
   }
 });
 
-router.delete("/:id", [auth], async (req, res) => {
+router.delete("/:id", async (req, res) => {
   if (!validateObjectId(req.params.id))
     return res.status(404).send("Invalid Object Id");
 
